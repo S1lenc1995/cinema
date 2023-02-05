a0bc42573490819633ddfab0fbc58bc5050ff8a6
Bohdan Kletskyi <underwater3499@gmail.com>
2023-02-05 14:50:40 +0200

    d

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,7 @@ import Favirites from "../pages/FavoritePage/FaviritePage";
 function RootRouters() {
     return (
     <Routes>
-        <Route path="/cinema" element={<HomePage />}/>
+        <Route path="#/cinema" element={<HomePage />}/>
         <Route path="/cinema/cinema/:id" element={<CinemaPage />}/>
         <Route path="/cinema/tv/:id" element={<TVPage />}/>
         <Route path="/cinema/favorites" element={<Favirites />}/>
